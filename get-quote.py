81560e14acf864852b3950074ef6b8c16dcff684
yesmaybenookay <paulsweany@protonmail.com>
2021-05-10 23:14:25 -0400

    Changed quotes to original quotes. Added function allowing users to add quotes to quotes.txt.

--- get-quote.py
+++ get-quote.py
@@ -7,7 +7,22 @@ def primary():
   last = len(quotes) - 1
   rnd = random.randint(0,last)
 
-  print(quotes[rnd])
+  print("\033[94m" + quotes[rnd], end="" + "\033[0m")
+
+def addQuote():
+  answer = None
+  while answer not in ("YES", "NO"):
+    answer = input("\033[91m" + "Do you want to add a new quote to this program? " + "\033[0m").upper()
+    if answer == "YES":
+      newQuote = input("\033[91m" + "Type the quote you want to add and then press Enter. " + "\033[0m") + "\n"
+      f = open("quotes.txt", "a")
+      f.write(newQuote)
+      f.close()
+    elif answer == "NO":
+      print("\033[91m" + "Fine. Be that way!" + "\033[0m")
+    else:
+      print("\033[91m" + "Please enter yes or no." + "\033[0m")
 
 if __name__== "__main__":
   primary()
+  addQuote()
